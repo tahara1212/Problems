469a331171a4cede5ae5ab2249f1db1d0bf0fb2c
shumpei.tahara <shumpei.tahara@nijibox.co.jp>
2022-10-08 23:00:33 +0900

    20221008

---
+++ code/202210/20221008/start/Example.js
@@ -0,0 +1,17 @@
+import Input from "./components/Input";
+import Select from "./components/Select";
+import Result from "./components/Result.js";
+import { CalcProvider } from "./context/CalcContext";
+
+const Example = () => {
+  return (
+    <CalcProvider>
+      <Input name="a" />
+      <Input name="b" />
+      <Select />
+      <Result />
+    </CalcProvider>
+  );
+};
+
+export default Example;
