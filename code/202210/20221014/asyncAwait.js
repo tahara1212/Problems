0f652745eea8fec3d8687d98a5042023bf26215e
shumpei.tahara <shumpei.tahara@nijibox.co.jp>
2022-10-14 23:36:36 +0900

    20221014

---
+++ code/202210/20221014/asyncAwait.js
@@ -0,0 +1,21 @@
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
+}
+
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  return val;
+}
+
+init().then(function (val) {
+  console.log("last " + val);
+});
