1fa498664d607c0d65de3d936821083d3847118a
shumpei.tahara <shumpei.tahara@nijibox.co.jp>
2023-03-16 20:59:59 +0900

    20230316

---
+++ code/202303/16.js
@@ -0,0 +1,10 @@
+const x = Number('3')
+// let x = require("fs").readFileSync("/dev/stdin", "utf8");
+
+const a = x * x * Math.PI
+const b = 2 * x * Math.PI
+console.log(`${a.toFixed(6)} ${b.toFixed(6)}`)
+
+/**
+ * 半径 r の円の面積と円周の長さを求めるプログラムを作成して下さい。
+ */
